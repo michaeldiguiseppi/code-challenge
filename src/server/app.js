b65b9279f4bd9861d267015cadbf2f260552cce2
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-04 18:50:01 -0600

    Adding satellizer.

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,69 @@
-// *** main dependencies *** //
+// // *** main dependencies *** //
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var qs = require('querystring');
+var async = require('async');
+var bcrypt = require('bcryptjs');
+var colors = require('colors');
+var cors = require('cors');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var request = require('request');
 
+var config = require('./config');
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, select: false },
+  displayName: String,
+  picture: String,
+  bitbucket: String,
+  facebook: String,
+  foursquare: String,
+  google: String,
+  github: String,
+  instagram: String,
+  linkedin: String,
+  live: String,
+  yahoo: String,
+  twitter: String,
+  twitch: String
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+mongoose.connect(config.MONGO_URI);
+mongoose.connection.on('error', function(err) {
+  console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+});
 
 // *** routes *** //
 var routes = require('./routes/index.js');
+var authRoutes = require('./routes/auth.js');
 
 
 // *** express instance *** //
@@ -16,15 +71,24 @@ var app = express();
 
 
 // *** config middleware *** //
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client')));
+// Force HTTPS on Heroku
+if (app.get('env') === 'production') {
+  app.use(function(req, res, next) {
+    var protocol = req.get('x-forwarded-proto');
+    protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
+  });
+}
+app.use(express.static(path.join(__dirname, '../../client')));
 
 
 // *** main routes *** //
 app.use('/', routes);
+app.use('/auth', authRoutes);
 
 
 // catch 404 and forward to error handler
